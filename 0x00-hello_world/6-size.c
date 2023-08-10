d095f417289eab5c2557709acdcae22e55fdf0e9
= <=>
2023-08-10 16:54:49 -0700

    new file 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Printing the size of the variouse types
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)" sizeof(char));
+	printf("Size of an int: %zu byte(s)" sizeof(int));
+	printf("Size of a long int: %zu byte(s)" sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)" sizeof(long long int));
+	printf("Size of a float: %zu byte(s)" sizeof(float));
+
+	return (0);
+}
