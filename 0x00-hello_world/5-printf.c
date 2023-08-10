e9a2aaeb3dba38d746c056334da3c0cccb4c561b
= <=>
2023-08-10 16:31:46 -0700

    new file 5-print.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Using printf to display a string
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
