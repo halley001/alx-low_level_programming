26e5294322022478a73a050e0abf86ddedf10fad
= <=>
2023-08-11 05:13:09 -0700

    program checking positive or negative numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
 #include <stdio.h>
+
+/* betty style doc for function main goes there */
 /**
  * main - Entry point
- *
- *Description: Check and print if number is positive, negative or zero.
- *
- * Return: Returns 0
+ * Return: Return 0
  */
 int main(void)
 {
 	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
 
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
-		{
-			printf("%d is negative\n", n);
-		}
-		else if (n == 0)
-			{
-				printf("%d is Zero\n", n);
-			}
-
-		return (0);
+	{
+		printf("%d is negative\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
 }
