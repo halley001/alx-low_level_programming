ca4b0ea124701403c08bfb75b88aef46ad6adf9c
= <=>
2023-08-11 22:16:32 -0700

    new filw 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Describtion: printing base 10 numbers from 0-9
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	printf("%d\n", i);
+	return (0);
+}
