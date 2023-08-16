d862d767f0a65efc256306c9bca962adc7f3ee18
= <=>
2023-08-16 16:07:13 -0700

    new file 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+        int n, harts = 0;
+
+        for (n = 0; n < 1024; n++)
+        {
+        if ((n % 3) == 0 || (n % 5) == 0)
+        {
+                harts += n;
+        }
+        }
+        printf("%d\n", harts);
+}
