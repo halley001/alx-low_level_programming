0e8d9eba3a86cdc06ceca900cc99ce0f098d469b
= <=>
2023-08-16 07:21:03 -0700

    new file 7- ..

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ *
+ * @n: takes value
+ *
+ * Return:  The value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int c = n % 10;
+
+	_putchar(c);
+	return (c);
+}
