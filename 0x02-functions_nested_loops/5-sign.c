c1677b97163836e7956408a5ea7367c879e99af1
= <=>
2023-08-16 02:21:39 -0700

    new file 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,16 +11,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar('\n');
 		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		_putchar('\n');
 		return (-1);
 	}
-	_putchar('0');
-	_putchar('\n');
 	return (0);
 }
