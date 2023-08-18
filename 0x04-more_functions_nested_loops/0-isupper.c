173e695129add1a164f6315e5ebb5431acee5a20
= <=>
2023-08-18 06:39:45 -0700

    fix @ Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,6 @@
 #include "main.h"
+
 /**
-<<<<<<< HEAD
-*_isdigit - function that verifies if a  character is a digit or not
-*@c: tested character
-*Return: returns 1 if it is uppercase , 0 if not
-*/
-=======
  * _isupper - Entry point
  *@c: parameter for comparism
  * Return: 1 if uppercase and 0 if lowercase
@@ -14,11 +9,6 @@
 int _isupper(int c)
 {
 if ((c >= 'A') && (c <= 'Z'))
->>>>>>> f770f863daca0637790367832bed81954324d3fb
-
-int _isdigit(int c)
-{
-if ((c >= 48) && (c <= 57))
 return (1);
 
 return (0);
