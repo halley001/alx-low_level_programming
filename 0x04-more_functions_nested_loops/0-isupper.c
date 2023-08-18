96a31c28c9a14552837017c6da74abfca521ed8d
= <=>
2023-08-18 04:28:49 -0700

    file main.h && 0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _isupper - Entry point
- *@c: parameter for comparism
- * Return: 1 if uppercase and 0 if lowercase
- */
-int _isupper(int c)
-{
-if ((c >= 'A') && (c <= 'Z'))
+*_isdigit - function that verifies if a  character is a digit or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
 
+int _isdigit(int c)
+{
+if ((c >= 48) && (c <= 57))
 return (1);
 
 return (0);
