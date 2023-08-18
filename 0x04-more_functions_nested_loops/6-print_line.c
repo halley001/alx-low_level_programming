428760aab02a8e33a1b5c37955add1e544b60298
= <=>
2023-08-18 14:14:00 -0700

    new file 6-print_lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - function that print a line in the terminal
+ * @c: integer parameter
+ * Return: returns void
+ */
+void print_line(int n)
+{
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar(10);
+}
