ad5d0f8cb1ef937bc283604906a53bc617eb22e3
= <=>
2023-08-18 08:47:53 -0700

    new 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul -  function that multiplies two integers
+ *
+ * @a: integer a
+ * @b: integer b
+ *
+ * Return: returns result
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
