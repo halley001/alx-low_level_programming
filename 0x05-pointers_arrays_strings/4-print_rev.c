3f716367e29c0b13ae2317c11de722bf27435ccb
= <=>
2023-08-22 13:07:33 -0700

    changes to file 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_rev - function that prints a string, in reverse, followed by a new line
- * @str: string
+ * print_rev - function that prints a string
+ * in reverse, followed by a new line
+ * @s: string
  * Return: reverse string
  */
 void print_rev(char *s)
