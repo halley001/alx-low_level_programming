92f2ad6984117b2363b7653b5b1f0c8aa2a3b05a
= <=>
2023-08-22 09:02:07 -0700

    main.h file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main.h - Defining function prototypes
+ * void: void functions
+ * int: interger function
+ * char: character function
+ */
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+#endif /*end main.h*/
