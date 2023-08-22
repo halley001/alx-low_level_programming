d7142415fc1a963d00c0b19183d15e6ef8f6fc33
= <=>
2023-08-22 13:03:25 -0700

    new file 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse, followed by a new line
+ * @str: string
+ * Return: reverse string
+ */
+void print_rev(char *s)
+{
+int count = _strlen(s);
+
+while (count > 1)
+{
+count--;
+_putchar(s[count]);
+}
+_putchar('\n');
+}
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+int count = 0;
+
+while (s[count] != '\0')
+{
+count++;
+}
+return (count);
+}
