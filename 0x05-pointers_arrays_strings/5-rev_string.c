4b374f5033365673ef7f5f82d3a2311154df69d9
= <=>
2023-09-27 07:03:07 -0700

    code correction

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * rev_string -  function that reverses a string
- * @s: variable
- * @Return: void
- */
+*rev_string - Reverse a string
+*@s: String to reverse
+*Return: Nothing
+*/
 void rev_string(char *s)
 {
 int i = 0, length;
@@ -17,17 +17,30 @@ length--;
 }
 
 /**
- * _strlen - function that returns the length of a string
+ *_strlen - returns the length of a string
  * @s: string
- * Return: string length
+ *Return: returns lenght;
  */
 int _strlen(char *s)
 {
-int count = 0;
+int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
 
-while (s[count] != '\0')
-{
-count++;
+return (inc);
 }
-return (count);
+
+/**
+*swap_char - swap two characters
+*@a: first character
+*@b: second character
+*Return: nothing
+*/
+
+void swap_char(char *a, char *b)
+{
+char tmp = *a;
+*a = *b;
+*b = tmp;
 }
